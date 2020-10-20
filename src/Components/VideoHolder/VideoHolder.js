15d0458c96b9bb144f82f79d4b5b8b5ef8480d91
Villy Devlioti <vadevlio@gmail.com>
2020-10-20 18:49:35 -0400

    toggle button on and off

--- src/Components/VideoHolder/VideoHolder.js
+++ src/Components/VideoHolder/VideoHolder.js
@@ -7,7 +7,6 @@ import "./VideoHolder.css";
 import Tooltips from './Tooltips/Tooltips.js'
 import animations from  '../../data.json'
 import ShuffleButton from './ShuffleButton/ShuffleButton.js'
-import BreakButton from '../../icons/BreakButton.svg'
 
 class VideoHolder extends Component {
     //Initialize stuff
@@ -15,54 +14,55 @@ class VideoHolder extends Component {
         super(props)
         this.state = {
             //variables which control video function and DOM elements
-            isHidden: false, //variable to render button
-            playNextVideo: true, // variable to play next video
+            isButtonHidden: false, //variable to render button
+            isVideoHidden: true, // variable to render video player
 
             //variables which control video information to show to DOM
             videoId: "",
             videoSrc: "",
-            author: ""       
+            author: "",   
+            
+            //json reading variable
+            animations
         }
         this.handleClick = this.handleClick.bind(this);
       }
     
     //trigger function 
     handleClick = () => {
-        this.setState({
-            playNextVideo: true,
-            isHidden: true
-        })
+        //toggle the state of the 'I Need a Break' Button
+        this.toggleStates(this.setState.isButtonHidden);
+
         //make a function to also make headline disappear
-        this.selectRandomAnimation (this.state.playNextVideo);
+        //this.selectRandomAnimation (this.state.playNextVideo);
     }
     
+    toggleStates = () => {
+        this.setState({
+            isButtonHidden: !this.state.isButtonHidden,
+        })
+        console.log("is it hidden?: ", this.state.isButtonHidden);
+    }
+
+    randomNumber = () => {
+        console.log("animations json file length: ", this.state.animations.length);
+        let videoChoice = Math.floor(Math.random() * this.state.animations.length) + 1;
+        console.log("next video up is: ", videoChoice)
+        return videoChoice;
+
+    }
+
     //self explanatory
     selectRandomAnimation = (playNextVideo) => {
-        let randomChoice = 0; // ancillary variable to help us change the state
-        let nextVideo = ""; //ancillary variable that will store the next video
-        let authorName = ""; //ancillary variable to store intermediate results
-        let videoQueueLength = animations.length; // reading from the file
-        console.log("ceiling is: ", videoQueueLength) // control point
+        //select random number
+        let nextVideo = this.randomNumber();
 
-        if (playNextVideo) { // has the button been clicked? If yes
-            randomChoice = (Math.floor(Math.random() * videoQueueLength)) + 1; //assign a new random choice for the video
-            console.log("next video up: ", randomChoice);
-//use map to read from file, based on randomChoice. Feed that data into the html return below
+        //play the video whose id corresponds to the random number
+        this.state.animations.includes(nextVideo)
 
-//            nextVideo = this.animations[randomChoice].source; //pick the next video
-//            authorName = animations[randomChoice].author; //assign the author for credit
-            this.setState({
-                videoId: randomChoice,
-                videoSrc : nextVideo,
-                author: authorName    
-            });
-            console.log("video id: ", this.state.videoId); //controlpoint
-        }
-        else { //else bye bye
-            console.log("no buttons clicked");
-            //add a function to make headline appear
-            return;
-        }
+        //after video ends make button visible again
+        this.toggleStates();
+
     }
 
     render () {
@@ -71,8 +71,7 @@ class VideoHolder extends Component {
             <Row className="video-row video-text">
                 {/*Shuffle Button Code*/}
                 <Button type="button" className="shuffle-button" onClick = {() => this.handleClick()}>
-                    {!this.state.isHidden && <ShuffleButton />} {/* only render button when video is not playing */}
-                    {/*on click this button will choose a different video which will be pushed to the video player*/}
+                    {!this.state.isButtonHidden && <ShuffleButton />} {/* only render button when video is not playing */}
                 </Button>
                 <Tooltips />
                 <Player playsInline poster="/assets/poster.png" src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4">
