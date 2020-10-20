97ba70374e073406a33786153a6890b7af0bde33
Villy Devlioti <vadevlio@gmail.com>
2020-10-20 17:55:56 -0400

    styling changes

--- src/Components/VideoHolder/VideoHolder.js
+++ src/Components/VideoHolder/VideoHolder.js
@@ -3,21 +3,25 @@ import Row from 'react-bootstrap/Row';
 import Button from 'react-bootstrap/Button';
 import { Player, ControlBar } from 'video-react';
 import "./VideoHolder.css";
-import MainHeadline from "./MainHeadline/MainHeadline";
+//import MainHeadline from "./MainHeadline/MainHeadline";
 import Tooltips from './Tooltips/Tooltips.js'
 import animations from  '../../data.json'
 import ShuffleButton from './ShuffleButton/ShuffleButton.js'
+import BreakButton from '../../icons/BreakButton.svg'
 
 class VideoHolder extends Component {
     //Initialize stuff
     constructor(props){
         super(props)
         this.state = {
-            playNextVideo: true,
+            //variables which control video function and DOM elements
+            isHidden: false, //variable to render button
+            playNextVideo: true, // variable to play next video
+
+            //variables which control video information to show to DOM
             videoId: "",
             videoSrc: "",
-            author: "", 
-            showHeadline: false // variable to hide headline
+            author: ""       
         }
         this.handleClick = this.handleClick.bind(this);
       }
@@ -25,7 +29,8 @@ class VideoHolder extends Component {
     //trigger function 
     handleClick = () => {
         this.setState({
-            playNextVideo: true
+            playNextVideo: true,
+            isHidden: true
         })
         //make a function to also make headline disappear
         this.selectRandomAnimation (this.state.playNextVideo);
@@ -64,12 +69,10 @@ class VideoHolder extends Component {
         //let videoChoice = this.props.videoChoice;  
         return (
             <Row className="video-row video-text">
-                <MainHeadline />
-                {/*<ShuffleButton />*/}
                 {/*Shuffle Button Code*/}
-                <Button type="button" className="btn btn-outline-primary shuffle-button" onClick = {() => this.handleClick()}>
+                <Button type="button" className="shuffle-button" onClick = {() => this.handleClick()}>
+                    {!this.state.isHidden && <ShuffleButton />} {/* only render button when video is not playing */}
                     {/*on click this button will choose a different video which will be pushed to the video player*/}
-                    <ShuffleButton />
                 </Button>
                 <Tooltips />
                 <Player playsInline poster="/assets/poster.png" src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4">
