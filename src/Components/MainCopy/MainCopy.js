44277ee029e374df31e5175243e19e6a30682a71
Villy Devlioti <vadevlio@gmail.com>
2020-10-25 18:59:52 -0400

    removed videos and adjusted css for safari

--- src/Components/MainCopy/MainCopy.js
+++ src/Components/MainCopy/MainCopy.js
@@ -5,9 +5,8 @@ import "./MainCopy.css";
 
 const MainCopy = () => 
     <Row className="main-copy">
-        <h1><p>Presidential elections are important. But our local community organizations are at it everyday, making tangible changes to people’s lives. </p>
-            <p>If you liked your brain break, we ask that you visit <a href="https://www.instagram.com/bedstuystrong/" target="_blank" rel="noopener noreferrer">Bed Stuy Strong</a> and learn more about how to help a grassroots 
-                <br />organization in the neighborhood that has welcomed us home. </p>
+        <h1><p>Presidential elections are important. But local politics are where the big things happen for our communities. </p>
+        <p>If you liked your momentary lapse, we ask that you visit <a href="https://www.instagram.com/softpowervote/" target="_blank" rel="noopener noreferrer">Soft Power Vote</a> and learn more about the issues down the block.</p>
     </h1>
     </Row>
 
